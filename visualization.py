f47d8e580254a7e1554db900708f1bb3604060da
SBL <boxianglyu@gmail.com>
2018-12-23 07:17:20 +0800

    Modified plots and plotting script

--- visualization.py
+++ visualization.py
@@ -9,6 +9,10 @@ i.e. lambda = 0, hence it requires n >> d.
 import numpy as np
 import scipy.linalg as slin
 import scipy.optimize as sopt
+import matplotlib.pyplot as plt
+
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def notears_simple(X: np.ndarray,
@@ -43,10 +47,52 @@ def notears_simple(X: np.ndarray,
         obj_grad = loss_grad + (rho * (np.trace(E) - d) + alpha) * E.T * W * 2
         return obj_grad.flatten()
 
+    def _visualize_func(w, title):
+        # Visualize each coordinate in the weight matrix
+        eps = np.linspace(-.5, .5, 1e4)
+        path = title + ".png"
+        fig, axes = plt.subplots(d, d, sharey = True)
+        w_shaped = w.reshape(d, d)
+        for i in range(d):
+            for j in range(d):
+                # pick the coordinate to be changed
+                xs = eps + w_shaped[i, j]
+                ys = np.zeros(len(eps))
+                for k in range(len(eps)):
+                    w_prime = w_shaped.copy()
+                    w_prime[i, j] += eps[k]
+                    ys[k] = _func(w_prime)
+                axes[i, j].plot(xs, ys)
+                ys.dump(title + "_" + str(i) + "_" + str(j) + ".npy")
+        fig.suptitle(title)
+        fig.savefig(path)
+
+    def _visualize_h(w, title):
+        # Visualize each coordinate in the weight matrix
+        eps = np.linspace(-.5, .5, 1e4)
+        path = title + ".png"
+        fig, axes = plt.subplots(d, d, sharey = True)
+        w_shaped = w.reshape(d, d)
+        for i in range(d):
+            for j in range(d):
+                # pick the coordinate to be changed
+                xs = eps + w_shaped[i, j]
+                ys = np.zeros(len(eps))
+                for k in range(len(eps)):
+                    w_prime = w_shaped.copy()
+                    w_prime[i, j] += eps[k]
+                    ys[k] = _func(w_prime)
+                axes[i, j].plot(xs, ys)
+        fig.suptitle(title)
+        fig.savefig(path)
+
     n, d = X.shape
     w_est, w_new = np.zeros(d * d), np.zeros(d * d)
     rho, alpha, h, h_new = 1.0, 0.0, np.inf, np.inf
+    w_time = None
+    pca_vis = PCA(n_components=2)
     bnds = [(0, 0) if i == j else (None, None) for i in range(d) for j in range(d)]
+    num_iter = 0
     for _ in range(max_iter):
         while rho < 1e+20:
             sol = sopt.minimize(_func, w_est, method='L-BFGS-B', jac=_grad, bounds=bnds)
@@ -57,10 +103,41 @@ def notears_simple(X: np.ndarray,
             else:
                 break
         w_est, h = w_new, h_new
+        num_iter += 1
+        if num_iter == 5:
+            _visualize_h(w_est.reshape([d, d]),
+                "Plot of constraint at 5th iteration")
+            _visualize_func(w_est.reshape([d, d]),
+                "Plot of function at 5th iteration")
+        if w_time is not None:
+            w_time = np.hstack((w_time, w_est[:, np.newaxis]))
+        else:
+            w_time = w_est[:, np.newaxis]
+            # First iteration
+
         alpha += rho * h
         if h <= h_tol:
             break
     w_est[np.abs(w_est) < w_threshold] = 0
+    _visualize_h(w_est.reshape([d, d]), "Plot of constraint at final solution")
+    _visualize_func(w_est.reshape([d, d]), "Plot of function at final solution")
+    w_time_pc = pca_vis.fit_transform(w_time)
+    eps = np.linspace(-.5, .5, 1e4)
+    cons_along_pc = np.zeros((int(1e4), int(1e4)))
+    func_along_pc = np.zeros((int(1e4), int(1e4)))
+    for i in range(len(eps)):
+        for j in range(len(eps)):
+            w_temp = w_est.copy()
+            w_temp += eps[i] * w_time_pc[:, 0]
+            w_temp += eps[j] * w_time_pc[:, 1]
+            cons_along_pc[i, j] = _h(w_temp[:, np.newaxis])
+            func_along_pc[i, j] = _func(w_temp[:, np.newaxis])
+
+    X, Y = np.meshgrid(eps, eps)
+    Axes3D.plot_surface(X, Y, cons_along_pc)
+    plt.show()
+    Axes3D.plot_surface(X, Y, func_along_pc)
+    plt.show()
     return w_est.reshape([d, d])
 
 
@@ -70,7 +147,7 @@ if __name__ == '__main__':
     import utils
 
     # configurations
-    n, d = 1000, 10
+    n, d = 1000, 5
     graph_type, degree, sem_type = 'erdos-renyi', 4, 'linear-gauss'
     log.info('Graph: %d node, avg degree %d, %s graph', d, degree, graph_type)
     log.info('Data: %d samples, %s SEM', n, sem_type)
