450bd5951535452533752cf7dd0d5c4b3559582a
SBL <boxianglyu@gmail.com>
2019-05-03 10:21:39 -0400

    update the plots

--- log_processing.py
+++ log_processing.py
@@ -29,7 +29,9 @@ time2_running_sum = 0
 time1_sum = 0
 time2_sum = 0
 time1_running_sum_list = []
+time1_accept_sum_list = []
 time2_running_sum_list = []
+time2_accept_sum_list = []
 w_change_list = []
 num_accept = 0
 num_reject = 0
@@ -42,16 +44,21 @@ for i in results_list:
         time2_running_sum += i[1]
         time2_sum += i[1]
     elif i[0] == "accepted" or i[0] == "rejected":
+
+        if i[0] == "accepted":
+            time1_accept_sum_list.append(time1_running_sum)
+            time2_accept_sum_list.append(time2_running_sum)
+            num_accept += 1
+        elif i[0] == "rejected":
+            num_reject += 1
+
         time1_running_sum_list.append(time1_running_sum)
         time1_running_sum = 0
         time2_running_sum_list.append(time2_running_sum)
         time2_running_sum = 0
-        if i[0] == "accepted":
-            num_accept += 1
-        elif i[0] == "rejected":
-            num_reject += 1
     else:
         w_change_list.append(i[1])
+
 
 plt.plot(np.arange(len(time1_running_sum_list)), time1_running_sum_list, label='Time1')
 plt.plot(np.arange(len(time2_running_sum_list)), time2_running_sum_list, label='Time2')
@@ -64,10 +71,20 @@ plt.savefig("time1_vs_time2.png")
 plt.clf()
 plt.cla()
 plt.plot(np.arange(len(w_change_list)), w_change_list)
-plt.xlabel("Number of Updates")
+plt.xlabel("Number of Accepted Updates")
 plt.ylabel("L2 norm of change")
 plt.title("Changes in W")
 plt.savefig("w_change.png")
+
+plt.clf()
+plt.cla()
+plt.plot(np.arange(len(time1_accept_sum_list)), time1_accept_sum_list, label='Time1')
+plt.plot(np.arange(len(time2_accept_sum_list)), time2_accept_sum_list, label='Time2')
+plt.legend()
+plt.xlabel("Number of Accepted Updates")
+plt.ylabel("Time (in seconds)")
+plt.title("Time1 vs Time2")
+plt.savefig("time1_vs_time2_accept.png")
 
 print("There are {} accepts and {} rejects".format(num_accept, num_reject))
 print("Proportion of accept is {}".format(num_accept/(num_accept + num_reject)))
